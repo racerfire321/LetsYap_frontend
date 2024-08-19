278eeab41877be81b8f5eceffcc26d8f27796105
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-08-19 12:12:39 +0545

    chore: add src folder with project structure

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import HomeScreen from './src/screens/HomeScreen'
 
-const App = () => {
+const HomeScreen = () => {
   return (
-    <HomeScreen/>
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
   )
 }
 
-export default App
+export default HomeScreen
 
 const styles = StyleSheet.create({})
