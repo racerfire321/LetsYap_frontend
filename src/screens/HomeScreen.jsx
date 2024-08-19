9baa4010703c8bfaf0ccdf269eb889d86f174e14
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-08-19 14:53:33 +0545

    feat: add header with icon

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Header from '../components/Header'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <View style={styles.container}>
+     <SafeAreaView  style={styles.area} >
+      <Header/>
+     </SafeAreaView>
+     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#1e1e1e',
+    padding: 20,
+  },
+  area:{
+    height: '100%',
+  }
+})
