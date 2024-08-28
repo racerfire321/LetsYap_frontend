47a6eb64737e3bca027e3185d996eec8e8936ae4
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-08-28 17:13:10 +0545

    feat: add event and notification

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Routes from './src/navigation/index';
 import { AuthProvider } from './src/contexts/auth/AuthProvider';
 import { ThemeProvider } from './src/contexts/theme/ThemeProvider';
+import { NotificationProvider } from './src/contexts/notification/NotificationContext';
 
 const App = () => {
+ 
   return (
     <ThemeProvider>
     <AuthProvider>
+    <NotificationProvider>
       <Routes />
+      </NotificationProvider>
     </AuthProvider>
     </ThemeProvider>
   );
